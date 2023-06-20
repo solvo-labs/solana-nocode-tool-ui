8752cc102174abd4fa7e7ca91e0edd8dd83ab168
ayseguleren <ayse.eren@windowslive.com>
2023-06-20 18:05:09 +0300

    creation TopBar and usage in ProtectedRoute

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+import { Grid, Theme } from "@mui/material";
+import TopBar from "../components/TopBar";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    color: "#FFFFFF",
+    justifyContent: "center",
+  },
+}));
 
 const ProtectedRoute: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Grid container spacing={2} className={classes.container}>
+      <TopBar />
+      <Grid item lg={10} md={12} xs={12}>
+        <Grid container direction={"column"} spacing={2}>
+          <Outlet />
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
