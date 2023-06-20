5835ce5aae42c7d331866ee94f4498f08d225781
ayseguleren <ayse.eren@windowslive.com>
2023-06-20 18:05:26 +0300

    App: addition mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Router from "./router/Router";
+import { ThemeProvider } from "@mui/styles";
+import { createTheme } from "@mui/material";
 
-const App = () => {
+const App: React.FC = () => {
+  const theme = createTheme();
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router />
-    </>
+    </ThemeProvider>
   );
 };
 
