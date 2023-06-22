282ca7b46a40cb94433f70d9b84f75edfdc58042
ayseguleren <ayse.eren@windowslive.com>
2023-06-22 14:04:13 +0300

    App: addition solana providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Router from "./router/Router";
 import { ThemeProvider } from "@mui/styles";
 import { createTheme } from "@mui/material";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl } from "@solana/web3.js";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const App: React.FC = () => {
   const theme = createTheme();
 
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [new UnsafeBurnerWalletAdapter()],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      <Router />
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <Router />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </ThemeProvider>
   );
 };
