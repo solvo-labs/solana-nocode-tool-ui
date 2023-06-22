7e39fcaa2fc2e0aa76ba650de50d788af8159b10
ayseguleren <ayse.eren@windowslive.com>
2023-06-22 15:16:27 +0300

    Login: css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,8 +2,27 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { Grid, Theme, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import solanaLogoPng from "../assets/solana-logo.png";
+import { APP_NAME } from "../utils/enum";
+
+const useStyles = makeStyles((_theme: Theme) => ({
+  outerContainer: {
+    padding: "1rem",
+    border: "1px solid linear-gradient(to right, #aa66fe, #23ed98)",
+    background: "linear-gradient(to right, #aa66fe, #23ed98)",
+    borderRadius: "0.5rem",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  innerContainer: { justifyContent: "center", alignItems: "center" },
+  image: { margin: "1.5rem 0", transition: "background-color 300ms" },
+  button: { background: "#CD2EF8" },
+}));
 
 const Login: React.FC = () => {
+  const classes = useStyles();
   const { connected } = useWallet();
   const navigate = useNavigate();
 
@@ -12,9 +31,18 @@ const Login: React.FC = () => {
   }
 
   return (
-    <div>
-      <WalletMultiButton />
-    </div>
+    <Grid container className={classes.outerContainer}>
+      <Grid container direction="column" className={classes.innerContainer}>
+        {/* #7846B5 */}
+        <Typography variant="h4" color="#5719A3">
+          {APP_NAME.SOLANA}
+        </Typography>
+        <img className={classes.image} width={200} src={solanaLogoPng} />
+      </Grid>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+        <WalletMultiButton className={classes.button} />
+      </div>
+    </Grid>
   );
 };
 
