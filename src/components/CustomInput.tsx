a6e907f536c8617b1b3d963df95e9f890f33fb93
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-07-07 03:46:12 +0300

    CustomInput created

---
+++ src/components/CustomInput.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { TextField, Theme } from "@mui/material";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  input: {
+    width: "100%",
+    // maxHeight: "44px !important",
+    [theme.breakpoints.up("sm")]: {
+      maxWidth: "18rem !important",
+    },
+    [theme.breakpoints.down("sm")]: {
+      maxWidth: "20rem !important",
+    },
+  },
+}));
+
+type Props = {
+  placeHolder: string;
+  label: string;
+  id: string;
+  name: string;
+  type: string;
+  value: string | number;
+  onChange: any;
+};
+
+export const CustomInput: React.FC<Props> = ({
+  placeHolder,
+  label,
+  id,
+  name,
+  type,
+  value,
+  onChange,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <TextField
+      className={classes.input}
+      placeholder={placeHolder}
+      id={id}
+      name={name}
+      label={label}
+      value={value}
+      onChange={onChange}
+      required
+      type={type}
+    ></TextField>
+  );
+};
