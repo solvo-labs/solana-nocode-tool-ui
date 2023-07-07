27adc829567d3402ac3fd365313898fd7d17624e
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-07-07 03:49:36 +0300

    TokenMint: tokendata state created, form created and manually specified properties replaced with tokendata

--- src/pages/Token/TokenMint.tsx
+++ src/pages/Token/TokenMint.tsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { createMint, getOrCreateAssociatedTokenAccount } from "../../lib/token";
 import { Transaction } from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID, createMintToInstruction, getAccount } from "@solana/spl-token";
 import { register } from "../../lib/tokenRegister";
+import { Token } from "../../utils/types";
+import { CustomInput } from "../../components/CustomInput";
+import { makeStyles } from "@mui/styles";
+import { Divider, Grid, Stack, Theme, Typography } from "@mui/material";
+import { CustomButton } from "../../components/CustomButton";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    textAlign: "center",
+    minWidth: "25rem",
+    [theme.breakpoints.down("sm")]: {
+      minWidth: "10rem",
+    },
+  },
+}));
 
 const TokenMint: React.FC = () => {
+  const classes = useStyles();
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
 
+  const [tokenData, setTokenData] = useState<Token>({
+    name: "",
+    symbol: "",
+    amount: 0,
+    decimal: 8,
+  });
+
   const createTransaction = async () => {
     if (publicKey) {
-      const { transaction, toAccount } = await createMint(connection, publicKey, 9);
+      const { transaction, toAccount } = await createMint(connection, publicKey, tokenData.decimal);
 
       const {
         context: { slot: minContextSlot },
@@ -29,11 +52,11 @@ const TokenMint: React.FC = () => {
         const account = await getAccount(connection, associatedToken, undefined, TOKEN_PROGRAM_ID);
 
         // supply
-        const transaction3 = new Transaction().add(createMintToInstruction(toAccount.publicKey, account.address, publicKey, 100000000000, [], TOKEN_PROGRAM_ID));
+        const transaction3 = new Transaction().add(createMintToInstruction(toAccount.publicKey, account.address, publicKey, tokenData.amount, [], TOKEN_PROGRAM_ID));
         const signature3 = await sendTransaction(transaction3, connection, { minContextSlot });
         await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature: signature3 });
 
-        const transaction4 = register(toAccount.publicKey.toBase58(), publicKey, { name: "test", symbol: "123" });
+        const transaction4 = register(toAccount.publicKey.toBase58(), publicKey, { name: tokenData.name, symbol: tokenData.symbol });
         const signature4 = await sendTransaction(transaction4, connection, { minContextSlot });
         await connection.confirmTransaction({ blockhash, lastValidBlockHeight, signature: signature4 });
       } catch (error) {
@@ -42,10 +65,76 @@ const TokenMint: React.FC = () => {
     }
   };
 
+  const disable = !(
+    tokenData.name &&
+    tokenData.symbol &&
+    tokenData.amount &&
+    tokenData.decimal
+  );
+
   return (
     <div>
-      <h1>TokenMint</h1>
-      <button onClick={createTransaction}></button>
+      <Grid container className={classes.container} direction={"column"}>
+        <Grid item marginBottom={"2rem"}>
+          <Typography variant="h5">Token Mint</Typography>
+          <Divider sx={{ marginTop: "1rem", background: "white" }} />
+        </Grid>
+        <Grid item justifyContent={"center"} marginBottom={"2rem"}>
+          <Stack direction={"column"} spacing={2} alignItems={"center"}>
+            <CustomInput
+              placeHolder="Name"
+              label="Name"
+              id="name"
+              name="name"
+              type="text"
+              value={tokenData.name}
+              onChange={(e: any) =>
+                setTokenData({ ...tokenData, name: e.target.value })
+              }
+            ></CustomInput>
+            <CustomInput
+              placeHolder="Symbol"
+              label="Symbol"
+              id="symbol"
+              name="symbol"
+              type="text"
+              value={tokenData.symbol}
+              onChange={(e: any) =>
+                setTokenData({ ...tokenData, symbol: e.target.value })
+              }
+            ></CustomInput>
+            <CustomInput
+              placeHolder="Amount"
+              label="Amount"
+              id="amount"
+              name="amount"
+              type="text"
+              value={tokenData.amount}
+              onChange={(e: any) =>
+                setTokenData({ ...tokenData, amount: e.target.value })
+              }
+            ></CustomInput>
+            <CustomInput
+              placeHolder="Decimal"
+              label="Decimal"
+              id="decimal"
+              name="decimal"
+              type="text"
+              value={tokenData.decimal}
+              onChange={(e: any) =>
+                setTokenData({ ...tokenData, decimal: e.target.value })
+              }
+            ></CustomInput>
+          </Stack>
+        </Grid>
+        <Grid item>
+          <CustomButton
+            label="create token"
+            disable={disable}
+            onClick={createTransaction}
+          ></CustomButton>
+        </Grid>
+      </Grid>
     </div>
   );
 };
