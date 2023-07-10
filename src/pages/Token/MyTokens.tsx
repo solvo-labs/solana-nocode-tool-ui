4d766d3cd3b89f47126a33d473ee19aaf85a9be6
afarukcali <afarukcali@gmail.com>
2023-07-10 17:52:13 +0300

    eslint warnings

--- src/pages/Token/MyTokens.tsx
+++ src/pages/Token/MyTokens.tsx
@@ -1,25 +1,13 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchUserTokens } from "../../lib";
-import {
-  CircularProgress,
-  Grid,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableFooter,
-  TableHead,
-  TablePagination,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { CircularProgress, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { Theme } from "@emotion/react";
 import { makeStyles } from "@mui/styles";
 import { TokenData } from "../../utils/types";
 
-const useStyles = makeStyles((theme: Theme) => ({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const useStyles = makeStyles((_theme: Theme) => ({
   container: {
     maxWidth: 700,
     justifyContent: "center",
@@ -44,7 +32,7 @@ export const MyTokens = () => {
   const { publicKey } = useWallet();
   const classes = useStyles();
   const [allToken, setAllToken] = useState<Array<TokenData>>([]);
-  const [actionLoader, setActionLoader] = useState<Boolean>(false);
+  const [actionLoader, setActionLoader] = useState<boolean>(false);
 
   useEffect(() => {
     setActionLoader(true);
@@ -61,6 +49,7 @@ export const MyTokens = () => {
   }, [connection, publicKey]);
 
   const listToken = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return allToken.map((a: any) => (
       <TableRow className={classes.tableRow}>
         <TableCell>{a.metadata.data.name}</TableCell>
@@ -90,25 +79,25 @@ export const MyTokens = () => {
   if (actionLoader) {
     return (
       <Grid container className={classes.container}>
-      <Grid item marginBottom={"3rem"}>
-        <Typography variant="h5">List of My Tokens</Typography>
+        <Grid item marginBottom={"3rem"}>
+          <Typography variant="h5">List of My Tokens</Typography>
+        </Grid>
+        <Grid item>
+          <TableContainer>
+            <Table component={Paper} sx={{ minWidth: 700 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.tableTitle}>Name</TableCell>
+                  <TableCell className={classes.tableTitle}>Symbol</TableCell>
+                  <TableCell className={classes.tableTitle}>Amount</TableCell>
+                  <TableCell className={classes.tableTitle}>Hex</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>{loader()}</TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
       </Grid>
-      <Grid item>
-        <TableContainer>
-          <Table component={Paper} sx={{ minWidth: 700 }}>
-            <TableHead>
-              <TableRow>
-                <TableCell className={classes.tableTitle}>Name</TableCell>
-                <TableCell className={classes.tableTitle}>Symbol</TableCell>
-                <TableCell className={classes.tableTitle}>Amount</TableCell>
-                <TableCell className={classes.tableTitle}>Hex</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>{loader()}</TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
-    </Grid>
     );
   }
 
