0637f8447575a74536e2624e1e9e79ae274ff2da
afarukcali <afarukcali@gmail.com>
2023-07-18 14:10:32 +0300

    Buffer fixed.

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "toastr/build/toastr.css";
+import { Buffer } from "buffer";
+
+window.Buffer = window.Buffer || Buffer;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
