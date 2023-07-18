d33032bfa2fc37aea6997772ce5be1ed121d7fba
afarukcali <afarukcali@gmail.com>
2023-07-18 11:51:06 +0300

    protected route public key check

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -18,12 +18,10 @@ const ProtectedRoute: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
 
   const { connected, publicKey } = useWallet();
+  console.log("connected", connected);
+  console.log("publicKey", publicKey);
 
   useEffect(() => {
-    if (publicKey === undefined || publicKey === null) {
-      setLoading(false);
-    }
-
     if (publicKey) {
       setLoading(false);
     }
