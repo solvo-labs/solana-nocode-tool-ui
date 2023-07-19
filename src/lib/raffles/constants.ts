f60e551be3f1ad465739f792e9812e13440dd71b
afarukcali <afarukcali@gmail.com>
2023-07-19 15:23:26 +0300

    Raffles design Fixes #13

---
+++ src/lib/raffles/constants.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const MASTER_SEED = "master";
+export const LOTTERY_SEED = "lottery";
+export const TICKET_SEED = "ticket";
+
+export const PROGRAM_ID = new PublicKey("ECWWMLCvKmp8bXAZ1B5iRotzRwEHdxa9tDjHCnD8XCzP");
