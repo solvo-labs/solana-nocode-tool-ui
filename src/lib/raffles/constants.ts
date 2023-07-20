afc08519224b1c894e026a82e531d22936f63285
afarukcali <afarukcali@gmail.com>
2023-07-20 13:31:03 +0300

    program updated.

--- src/lib/raffles/constants.ts
+++ src/lib/raffles/constants.ts
@@ -4,4 +4,4 @@ export const MASTER_SEED = "master";
 export const LOTTERY_SEED = "lottery";
 export const TICKET_SEED = "ticket";
 
-export const PROGRAM_ID = new PublicKey("ECWWMLCvKmp8bXAZ1B5iRotzRwEHdxa9tDjHCnD8XCzP");
+export const PROGRAM_ID = new PublicKey("FiafUCcKJrc69chmjw1zjeyAS1aDvmRxaCARxoZBJKPZ");
