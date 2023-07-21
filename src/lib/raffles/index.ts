e3a6874cdc2c4f12db9f667ae676f5b75dc6973f
afarukcali <afarukcali@gmail.com>
2023-07-21 15:20:04 +0300

    build

--- src/lib/raffles/index.ts
+++ src/lib/raffles/index.ts
@@ -1,6 +1,5 @@
-import { BN, Idl, Program } from "@project-serum/anchor";
-import { LAMPORTS_PER_SOL, PublicKey, SystemProgram } from "@solana/web3.js";
-import { getLotteryAddress } from "./program";
+import { Idl, Program } from "@project-serum/anchor";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 
 export const initMaster = async (program: Program<Idl>, masterAddress: PublicKey, owner: PublicKey) => {
   const txHash = await program.methods
