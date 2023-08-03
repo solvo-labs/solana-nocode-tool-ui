38cdf5d482b0affb76e7b821ec0510f21321bc8c
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-08-03 10:23:29 +0300

    mainpage redesing

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Grid, Theme } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Profile from "../components/Profile";
+import Network from "../components/Network";
+import SolPrice from "../components/SolPrice";
+import CurrentBlock from "../components/CurrentBlock";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { makeStyles } from "@mui/styles";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    minWidth: "80vw",
+    [theme.breakpoints.down("sm")]: {
+      minWidth: "80vw",
+    },
+  },
+}));
 
 const Main: React.FC = () => {
-  return <div>Main Page</div>;
+  const { publicKey } = useWallet();
+  const classes = useStyles();
+  const { connection } = useConnection();
+  const [balance, setBalance] = useState<number>();
+
+  useEffect(() => {
+    const init = async () => {
+      if (publicKey) {
+        const balance = await connection.getBalance(publicKey);
+        const lastBalance = balance / LAMPORTS_PER_SOL
+        setBalance(lastBalance);
+      }
+    }
+    init();
+  }, [connection, publicKey])
+  
+  
+  return (
+    <Grid container spacing={2} className={classes.container}>
+      <Grid item xl={4} lg={4} md={6}>
+        <SolPrice />
+      </Grid>
+      <Grid item xl={4} lg={4} md={6}>
+        <Profile
+          balance={balance ? balance : 0}
+          publicKey={
+            publicKey?.toBase58() ? publicKey.toBase58() : "Connect wallet"
+          }
+        ></Profile>
+      </Grid>
+      <Grid item xl={4} lg={4} md={12}>
+        <CurrentBlock></CurrentBlock>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Main;
