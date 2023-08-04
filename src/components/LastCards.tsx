4208c5e122149e6842409f94e275e890dced69da
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-08-04 10:14:05 +0300

    Main: updated main

---
+++ src/components/LastCards.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import React from "react";
+
+const LastCard = () => {
+    return (
+        <Card sx={{height:"200px", borderRadius: "16px"}}>
+            <CardContent>
+                <Typography>aaaaaaaaaaaaaa</Typography>
+            </CardContent>
+        </Card>
+    )
+};
+
+export default LastCard;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,18 +1,27 @@
 import { Grid, Theme } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import Profile from "../components/Profile";
-import Network from "../components/Network";
 import SolPrice from "../components/SolPrice";
 import CurrentBlock from "../components/CurrentBlock";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { makeStyles } from "@mui/styles";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import WalletDetail from "../components/WalletDetail";
+import SolNetwork from "../components/SolNetwork";
+import SolTotalStake from "../components/SolTotalStake";
+import SolSupply from "../components/SolSupply";
+import LastCard from "../components/LastCards";
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
-    minWidth: "80vw",
+    maxWidth: "80vw",
+    marginBottom: "1rem !important",
+    [theme.breakpoints.down("xl")]: {
+      maxWidth: "90vw",
+      marginTop: "1rem !important",
+    },
     [theme.breakpoints.down("sm")]: {
-      minWidth: "80vw",
+      marginTop: "2rem !important",
     },
   },
 }));
@@ -22,34 +31,62 @@ const Main: React.FC = () => {
   const classes = useStyles();
   const { connection } = useConnection();
   const [balance, setBalance] = useState<number>();
+  const [walletConnection, setWalletConnection] =
+    useState<string>("not connected");
 
   useEffect(() => {
     const init = async () => {
       if (publicKey) {
         const balance = await connection.getBalance(publicKey);
-        const lastBalance = balance / LAMPORTS_PER_SOL
+        const lastBalance = balance / LAMPORTS_PER_SOL;
         setBalance(lastBalance);
+        setWalletConnection("connected");
       }
-    }
+    };
     init();
-  }, [connection, publicKey])
-  
-  
+  }, [connection, publicKey]);
+
   return (
     <Grid container spacing={2} className={classes.container}>
-      <Grid item xl={4} lg={4} md={6}>
-        <SolPrice />
+      {/* --------------------------------------------------------------------------------------------- en dis grid */}
+      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+        <Grid container spacing={2}>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            <Profile
+              walletConnection={walletConnection}
+              balance={balance ? balance : 0}
+              publicKey={
+                publicKey?.toBase58() ? publicKey.toBase58() : "Connect wallet"
+              }
+            ></Profile>
+          </Grid>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            <WalletDetail></WalletDetail>
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item xl={4} lg={4} md={6}>
-        <Profile
-          balance={balance ? balance : 0}
-          publicKey={
-            publicKey?.toBase58() ? publicKey.toBase58() : "Connect wallet"
-          }
-        ></Profile>
-      </Grid>
-      <Grid item xl={4} lg={4} md={12}>
-        <CurrentBlock></CurrentBlock>
+      {/* --------------------------------------------------------------------------------------------- en dis grid */}
+      <Grid item xl={8} lg={8} md={6} sm={12} xs={12}>
+        <Grid container spacing={2}>
+          <Grid item xl={6} lg={6} md={12} sm={12} xs={12}>
+            <SolPrice />
+          </Grid>
+          <Grid item xl={6} lg={6} md={12} sm={12} xs={12}>
+            <CurrentBlock></CurrentBlock>
+          </Grid>
+          <Grid item xl={4} lg={4} md={12} sm={12} xs={12}>
+            <SolNetwork></SolNetwork>
+          </Grid>
+          <Grid item xl={4} lg={4} md={12} sm={12} xs={12}>
+            <SolTotalStake></SolTotalStake>
+          </Grid>
+          <Grid item xl={4} lg={4} md={12} sm={12} xs={12}>
+            <SolSupply></SolSupply>
+          </Grid>
+          <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
+            <LastCard></LastCard>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
