ec8a78787206ce8131e8083aabad1d939976d0e2
afarukcali <afarukcali@gmail.com>
2023-08-09 21:20:45 +0300

    vesting outgoing list

--- src/pages/Tokenomics/VestingList.tsx
+++ src/pages/Tokenomics/VestingList.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
 import React, { useEffect, useState } from "react";
-import { getVestingMyIncoming, unlock, withdraw } from "../../lib/vesting";
+import { getVestingMyIncoming, getVestingMyOwn, unlock, withdraw } from "../../lib/vesting";
 import { Stream } from "@streamflow/stream/dist/solana";
 import {
+  Box,
   CircularProgress,
   Divider,
   Grid,
   Paper,
+  Tab,
   Table,
   TableBody,
   TableCell,
@@ -28,6 +30,7 @@ import { PublicKey } from "@solana/web3.js";
 import { getMetadataPDA } from "../../lib/tokenRegister";
 import { UnlockSchedule, UnlockScheduleType } from "../../lib/models/Vesting";
 import { CustomButton } from "../../components/CustomButton";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const useStyles = makeStyles((_theme: Theme) => ({
@@ -96,8 +99,10 @@ export const VestingList = () => {
   const { publicKey } = useWallet();
   const [loading, setLoading] = useState<boolean>(true);
   const [vestingList, setVestingList] = useState<[string, Stream][]>([]);
+  const [outgoingvestingList, setOutgoingVestingList] = useState<[string, Stream][]>([]);
   const classes = useStyles();
   const { connection } = useConnection();
+  const [activeTab, setActiveTab] = useState<string>("1");
   const wallet = useAnchorWallet();
 
   const [page, setPage] = useState(0);
@@ -116,13 +121,14 @@ export const VestingList = () => {
     const init = async () => {
       if (publicKey) {
         const data = await getVestingMyIncoming(publicKey.toBase58());
+        const outgoingData = await getVestingMyOwn(publicKey.toBase58());
 
         const sortedData = data?.sort((a, b) => a[1].createdAt - b[1].createdAt);
-
-        const decimalPromises: any[] = [];
-        const metadataPromises: any[] = [];
+        const sortedOutgoingData = outgoingData?.sort((a, b) => a[1].createdAt - b[1].createdAt);
 
         if (sortedData) {
+          const decimalPromises: any[] = [];
+          const metadataPromises: any[] = [];
           sortedData.forEach((dt) => {
             decimalPromises.push(connection.getTokenSupply(new PublicKey(dt[1].mint)));
             metadataPromises.push(getMetadataPDA(new PublicKey(dt[1].mint), connection));
@@ -135,12 +141,29 @@ export const VestingList = () => {
             return { ...st, decimal: decimalsData[index].value.decimals, metadata: metadata[index] };
           });
 
-          console.log(finalData);
-
           setVestingList(finalData || []);
-          setLoading(false);
+        }
+
+        if (sortedOutgoingData) {
+          const decimalPromises: any[] = [];
+          const metadataPromises: any[] = [];
+          sortedOutgoingData.forEach((dt) => {
+            decimalPromises.push(connection.getTokenSupply(new PublicKey(dt[1].mint)));
+            metadataPromises.push(getMetadataPDA(new PublicKey(dt[1].mint), connection));
+          });
+
+          const decimalsData = await Promise.all(decimalPromises);
+          const metadata = await Promise.all(metadataPromises);
+
+          const finalData = sortedOutgoingData.map((st: any, index: number) => {
+            return { ...st, decimal: decimalsData[index].value.decimals, metadata: metadata[index] };
+          });
+
+          setOutgoingVestingList(finalData || []);
         }
       }
+
+      setLoading(false);
     };
 
     init();
@@ -202,8 +225,8 @@ export const VestingList = () => {
     );
   };
 
-  const listVesting = () => {
-    return vestingList?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)?.map((e: any, index: number) => (
+  const listVesting = (list: any, isOutgoing = false) => {
+    return list?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)?.map((e: any, index: number) => (
       <TableRow className={classes.tableRow} key={index}>
         <TableCell>{e[1].name}</TableCell>
         <TableCell align="center">{getStatusIcon(e[1].start, e[1].end)}</TableCell>
@@ -217,17 +240,19 @@ export const VestingList = () => {
         <TableCell>{timestampToDate(e[1].cliff)}</TableCell>
         <TableCell align="center">{e[1].cliffAmount.toNumber() / Math.pow(10, e[1].cliffAmount.length)}</TableCell>
         <TableCell align="center">{e[1].automaticWithdrawal ? "YES" : "NO"}</TableCell>
-        <TableCell align="center">
-          {!e[1].automaticWithdrawal && e[1].withdrawnAmount.toNumber() !== e[1].depositedAmount.toNumber() && getTimestamp() >= e[1].start && getTimestamp() <= e[1].end && (
-            <CustomButton
-              onClick={() => {
-                withdrawToken(e[0], e.decimal);
-              }}
-              label={"Claim"}
-              disable={false}
-            />
-          )}
-        </TableCell>
+        {!isOutgoing && (
+          <TableCell align="center">
+            {!e[1].automaticWithdrawal && e[1].withdrawnAmount.toNumber() !== e[1].depositedAmount.toNumber() && getTimestamp() >= e[1].start && getTimestamp() <= e[1].end && (
+              <CustomButton
+                onClick={() => {
+                  withdrawToken(e[0], e.decimal);
+                }}
+                label={"Claim"}
+                disable={false}
+              />
+            )}
+          </TableCell>
+        )}
       </TableRow>
     ));
   };
@@ -249,76 +274,162 @@ export const VestingList = () => {
   }
 
   return (
-    vestingList && (
-      <Grid container direction={"row"} className={classes.container}>
-        <Grid item className={classes.titleContainer}>
-          <Typography variant="h5">My Incoming Vesting's</Typography>
-          <Divider sx={{ marginTop: "1rem", background: "white" }} />
-        </Grid>
-        <Grid container marginTop={"2rem"}>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Name
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Status
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Start
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    End
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Last Withdrawn At
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Mint
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Period
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Withdrawn Amount
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Deposited Amount
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Cliff
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Cliff Amount
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Automatic Withdrawal
-                  </TableCell>
-                  <TableCell align="center" className={classes.tableTitle}>
-                    Claim
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>{listVesting()}</TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-        <Grid container className={classes.paginatonContainer} sx={{}}>
-          <TablePagination
-            className={classes.pagination}
-            rowsPerPageOptions={[1, 5, 10]}
-            component="div"
-            colSpan={4}
-            count={vestingList.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Grid>
-      </Grid>
-    )
+    <>
+      <TabContext value={activeTab}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider", width: "100%", display: "flex", justifyContent: "center" }}>
+          <TabList
+            onChange={(_event: React.SyntheticEvent, newValue: string) => {
+              setActiveTab(newValue);
+            }}
+          >
+            <Tab label="My Incoming Vesting's" value="1" />
+            <Tab label="My Outgoing Vesting's" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          {vestingList && (
+            <Grid container direction={"row"} className={classes.container}>
+              <Grid item className={classes.titleContainer}>
+                <Typography variant="h5">My Incoming Vesting's</Typography>
+                <Divider sx={{ marginTop: "1rem", background: "white" }} />
+              </Grid>
+              <Grid container marginTop={"2rem"}>
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Name
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Status
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Start
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          End
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Last Withdrawn At
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Mint
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Period
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Withdrawn Amount
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Deposited Amount
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Cliff
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Cliff Amount
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Automatic Withdrawal
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Claim
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>{listVesting(vestingList)}</TableBody>
+                  </Table>
+                </TableContainer>
+              </Grid>
+              <Grid container className={classes.paginatonContainer} sx={{}}>
+                <TablePagination
+                  className={classes.pagination}
+                  rowsPerPageOptions={[1, 5, 10]}
+                  component="div"
+                  colSpan={4}
+                  count={vestingList.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </Grid>
+            </Grid>
+          )}
+        </TabPanel>
+        <TabPanel value="2">
+          {outgoingvestingList && (
+            <Grid container direction={"row"} className={classes.container}>
+              <Grid item className={classes.titleContainer}>
+                <Typography variant="h5">My Outgoing Vesting's</Typography>
+                <Divider sx={{ marginTop: "1rem", background: "white" }} />
+              </Grid>
+              <Grid container marginTop={"2rem"}>
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Name
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Status
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Start
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          End
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Last Withdrawn At
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Mint
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Period
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Withdrawn Amount
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Deposited Amount
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Cliff
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Cliff Amount
+                        </TableCell>
+                        <TableCell align="center" className={classes.tableTitle}>
+                          Automatic Withdrawal
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>{listVesting(outgoingvestingList, true)}</TableBody>
+                  </Table>
+                </TableContainer>
+              </Grid>
+              <Grid container className={classes.paginatonContainer} sx={{}}>
+                <TablePagination
+                  className={classes.pagination}
+                  rowsPerPageOptions={[1, 5, 10]}
+                  component="div"
+                  colSpan={4}
+                  count={outgoingvestingList.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </Grid>
+            </Grid>
+          )}
+        </TabPanel>
+      </TabContext>
+    </>
   );
 };
