4ffd53f918cc9e5bf8f572552a41d21ba9621f93
ayseguleren <ayse.eren@windowslive.com>
2023-08-14 21:51:23 +0300

    CreateDao: using steps and some css

--- src/pages/Dao/CreateDao.tsx
+++ src/pages/Dao/CreateDao.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Divider, Grid, Theme, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import BusinessIcon from "@mui/icons-material/Business";
@@ -10,35 +10,41 @@ import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
 import DaoCategories from "../../components/DaoCategories";
 import { Category } from "../../utils/types";
+import { Steps } from "../../components/DaoStep/Steps";
+import { DAO_STEPS } from "../../utils/enum";
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
-    minWidth: "30vw",
+    padding: "2rem",
+    [theme.breakpoints.up("sm")]: {
+      minWidth: "30vw",
+    },
     [theme.breakpoints.down("sm")]: {
       minWidth: "80vw",
     },
   },
+  top: {
+    position: "fixed",
+    top: 100,
+    left: 0,
+    width: "100%",
+    padding: "1rem 0rem 0.5rem 0rem",
+    background: "linear-gradient(to left, #aa66fe, #23ed98)",
+  },
   title: {
     textAlign: "center",
-  },
-  select: {
-    width: "100%",
-  },
-  boxContainer: {
-    width: "100%",
-    margin: "1rem 0",
-  },
-  box: {
-    width: "5rem",
   },
   daoCategories: {
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
     alignItems: "center",
+    marginTop: "7rem",
+    overflow: "auto",
     "& > div": {
-      width: "calc(50% - 0.5rem)", // Yarı genişlikteki iki sütunlu düzen
+      width: "48%",
       marginBottom: "1rem",
+      marginTop: "1rem",
     },
   },
 }));
@@ -82,16 +88,19 @@ const categories: Category[] = [
 ];
 
 const CreateDao: React.FC = () => {
+  const [activeStep, setActiveStep] = useState<number>(1);
+
   const classes = useStyles();
 
   return (
-    <Grid container className={classes.container} direction={"column"}>
-      <Grid item marginBottom={"1rem"}>
+    <Grid container className={classes.container} direction="column">
+      <div className={classes.top}>
         <Typography variant="h5" className={classes.title}>
           Create Dao
         </Typography>
-        <Divider sx={{ marginTop: "0.5rem", background: "white", marginBottom: "2rem" }} />
-      </Grid>
+        <Divider sx={{ background: "white" }} />
+        <Steps activeStep={activeStep} allSteps={Object.values(DAO_STEPS)} />
+      </div>
       <div className={classes.daoCategories}>
         {categories.map((category) => (
           <DaoCategories key={category.id} label={category.label} index={category.id} icon={category.icon} />
