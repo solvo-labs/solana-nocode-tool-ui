b65b545d34cdd02d7a9f627051dc41631bf83a9d
ayseguleren <ayse.eren@windowslive.com>
2023-08-14 21:51:05 +0300

    creation Steps and CustomSteps

---
+++ src/components/DaoStep/Steps.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+import { Theme } from "@mui/material";
+import { CustomStep } from "./CustomStep";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  appBar: {
+    marginTop: "2rem",
+    width: "100%",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "2rem",
+    },
+  },
+  cardDiv: {
+    marginTop: "8rem",
+    display: "flex",
+    alignItem: "center",
+    justifyContent: "center",
+  },
+}));
+
+type Props = {
+  allSteps: string[];
+  activeStep: number;
+};
+
+export const Steps: React.FC<Props> = ({ allSteps, activeStep }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.appBar}>
+      <CustomStep steps={allSteps} activeStep={activeStep} />
+    </div>
+  );
+};
