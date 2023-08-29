eeef1e23040f09456a7e0ffb3d48fc824d6d6246
ayseguleren <ayse.eren@windowslive.com>
2023-08-30 00:23:37 +0300

    enums: creation TOKEN_TYPES

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -22,3 +22,10 @@ export enum DAO_STEPS {
   TOKEN_INFO = "Token Detail",
   REVIEW = "Review",
 }
+
+export enum TOKEN_TYPES {
+  NEW_TOKEN = "New Token",
+  TOKEN_FROM_WALLET = "Token from Wallet",
+  NEW_NFT = "New Nft",
+  NFT_FROM_WALLET = "Nft from Wallet",
+}
