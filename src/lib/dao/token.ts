4a1f89dc203a84336b4e8f8758a821c9f0930df5
afarukcali <afarukcali@gmail.com>
2023-10-11 15:19:08 +0300

    realm governance ui with ref // ref https://github.com/solana-labs/governance-ui

--- src/lib/dao/token.ts
+++ src/lib/dao/token.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { MintLayout, TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+
+// ref https://github.com/solana-labs/governance-ui
+import { ASSOCIATED_TOKEN_PROGRAM_ID, MintLayout, TOKEN_PROGRAM_ID, Token, u64 } from "@solana/spl-token";
 import { Connection, Keypair, PublicKey, SystemProgram, TransactionInstruction } from "@solana/web3.js";
 
 export const withCreateMint = async (
@@ -29,4 +32,57 @@ export const withCreateMint = async (
 
   instructions.push(Token.createInitMintInstruction(TOKEN_PROGRAM_ID, mintAccount.publicKey, decimals, ownerPk, freezeAuthorityPk));
   return mintAccount.publicKey;
+};
+
+export async function tryGetMint(connection: Connection, publicKey: PublicKey) {
+  try {
+    const result = await connection.getAccountInfo(publicKey);
+    const data = Buffer.from(result!.data);
+    const account = parseMintAccountData(data);
+    return {
+      publicKey,
+      account,
+    };
+  } catch (ex) {
+    console.error(`Can't fetch mint ${publicKey?.toBase58()} @ ${connection.rpcEndpoint}`, ex);
+    return undefined;
+  }
+}
+
+export function parseMintAccountData(data: Buffer) {
+  const mintInfo = MintLayout.decode(data) as unknown as any;
+
+  if (mintInfo.mintAuthorityOption === 0) {
+    mintInfo.mintAuthority = null;
+  } else {
+    mintInfo.mintAuthority = new PublicKey(mintInfo.mintAuthority);
+  }
+
+  mintInfo.supply = u64.fromBuffer(mintInfo.supply);
+  mintInfo.isInitialized = mintInfo.isInitialized != 0;
+
+  if (mintInfo.freezeAuthorityOption === 0) {
+    mintInfo.freezeAuthority = null;
+  } else {
+    mintInfo.freezeAuthority = new PublicKey(mintInfo.freezeAuthority);
+  }
+  return mintInfo;
+}
+
+export const withCreateAssociatedTokenAccount = async (instructions: TransactionInstruction[], mintPk: PublicKey, ownerPk: PublicKey, payerPk: PublicKey) => {
+  const ataPk = await Token.getAssociatedTokenAddress(
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    TOKEN_PROGRAM_ID,
+    mintPk,
+    ownerPk, // owner
+    true
+  );
+
+  instructions.push(Token.createAssociatedTokenAccountInstruction(ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, mintPk, ataPk, ownerPk, payerPk));
+
+  return ataPk;
+};
+
+export const withMintTo = async (instructions: TransactionInstruction[], mintPk: PublicKey, destinationPk: PublicKey, mintAuthorityPk: PublicKey, amount: number | u64) => {
+  instructions.push(Token.createMintToInstruction(TOKEN_PROGRAM_ID, mintPk, destinationPk, mintAuthorityPk, [], amount));
 };
--- src/lib/dao/utils.ts
+++ src/lib/dao/utils.ts
@@ -1,23 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // REF https://github.com/nation-io/solana-dao-sdk/blob/main/packages/solana-dao-sdk/src/internal/services/daoService.ts
+// ref https://github.com/solana-labs/governance-ui
 
-import { BlockhashWithExpiryBlockHeight, Connection, PublicKey, Transaction } from "@solana/web3.js";
+import { BlockhashWithExpiryBlockHeight, Connection, Keypair, PublicKey, Transaction, TransactionInstruction } from "@solana/web3.js";
 import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from "../token";
 import { Wallet } from "@project-serum/anchor/dist/cjs/provider";
 import { BN } from "@project-serum/anchor";
 import {
   GovernanceConfig,
+  GoverningTokenConfigAccountArgs,
+  MintMaxVoteWeightSource,
+  MintMaxVoteWeightSourceType,
   SetRealmAuthorityAction,
   VoteThreshold,
   VoteThresholdType,
   VoteTipping,
+  getGovernanceProgramVersion,
   getTokenOwnerRecordAddress,
+  withCreateGovernance,
   withCreateMintGovernance,
+  withCreateNativeTreasury,
   withCreateRealm,
+  withCreateTokenOwnerRecord,
   withDepositGoverningTokens,
   withSetRealmAuthority,
 } from "@solana/spl-governance";
-import { DEFAULT_COMMUNITY_MINT_MAX_VOTE_WEIGHT_SOURCE, GOVERNANCE_PROGRAM_ID, MIN_COMMUNITY_TOKENS_TO_CREATE_WITH_ZERO_SUPPLY } from "./constants";
+import { DEFAULT_COMMUNITY_MINT_MAX_VOTE_WEIGHT_SOURCE, DISABLED_VOTER_WEIGHT, GOVERNANCE_PROGRAM_ID, MIN_COMMUNITY_TOKENS_TO_CREATE_WITH_ZERO_SUPPLY } from "./constants";
 import BigNumber from "bignumber.js";
+import { tryGetMint, withCreateAssociatedTokenAccount, withCreateMint, withMintTo } from "./token";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
 
 export const mintCommunityToken = async (connection: Connection, wallet: Wallet, recentBlockhash: BlockhashWithExpiryBlockHeight, decimal = 8) => {
   const communityMint = await createMint(connection, wallet.publicKey, wallet.publicKey, decimal);
@@ -247,3 +261,269 @@ const getTimestampFromDays = (days: number) => {
 
   return days * SECONDS_PER_DAY;
 };
+
+export const createCommunityDao = async (
+  connection: Connection,
+  walletPk: PublicKey,
+  realmName: string,
+  useSupplyFactor: boolean,
+  createCouncil: boolean,
+  councilWalletPks: PublicKey[],
+  communityYesVotePercentage: "disabled" | number,
+  councilYesVotePercentage: "disabled" | number,
+  transferCommunityMintAuthority: boolean,
+  transferCouncilMintAuthority: boolean,
+  existingCommunityMintPk?: PublicKey,
+  existingCouncilMintPk?: PublicKey,
+  communityMintSupplyFactor?: number,
+  communityAbsoluteMaxVoteWeight?: number,
+  tokensToGovernThreshold?: number,
+  communityTokenConfig?: GoverningTokenConfigAccountArgs,
+  councilTokenConfig?: GoverningTokenConfigAccountArgs,
+  maxVotingTimeInDays?: number,
+  skipRealmAuthority?: boolean
+) => {
+  const realmInstructions: TransactionInstruction[] = [];
+  const realmSigners: Keypair[] = [];
+
+  const mintsSetupInstructions: TransactionInstruction[] = [];
+  const councilMembersInstructions: TransactionInstruction[] = [];
+
+  const mintsSetupSigners: Keypair[] = [];
+  const initialCouncilTokenAmount = 1;
+
+  const programIdPk = GOVERNANCE_PROGRAM_ID;
+
+  const programVersion = await getGovernanceProgramVersion(connection, programIdPk);
+
+  const communityMintAccount = existingCommunityMintPk && (await tryGetMint(connection, existingCommunityMintPk));
+  const zeroCommunityTokenSupply = existingCommunityMintPk ? communityMintAccount?.account.supply.isZero() : true;
+  const communityMintDecimals = communityMintAccount?.account?.decimals || 6;
+
+  const communityMaxVoteWeightSource = parseMintMaxVoteWeight(useSupplyFactor, communityMintDecimals, communityMintSupplyFactor, communityAbsoluteMaxVoteWeight);
+
+  const councilMintAccount = existingCouncilMintPk && (await tryGetMint(connection, existingCouncilMintPk));
+  const zeroCouncilTokenSupply = existingCouncilMintPk ? councilMintAccount?.account.supply.isZero() : true;
+  const councilMintHasMintAuthority = councilMintAccount ? !!councilMintAccount.account.mintAuthority : true;
+
+  let communityMintPk = existingCommunityMintPk;
+
+  if (!communityMintPk) {
+    // Create community mint
+    communityMintPk = await withCreateMint(connection, mintsSetupInstructions, mintsSetupSigners, walletPk, null, communityMintDecimals, walletPk);
+  }
+
+  let councilMintPk;
+
+  if (zeroCommunityTokenSupply && zeroCouncilTokenSupply) {
+    throw new Error("no tokens exist that could govern this DAO");
+  }
+
+  if (!existingCouncilMintPk && createCouncil) {
+    councilMintPk = await withCreateMint(connection, mintsSetupInstructions, mintsSetupSigners, walletPk, null, 0, walletPk);
+  } else {
+    councilMintPk = existingCouncilMintPk;
+  }
+
+  // Convert to mint natural amount
+  const minCommunityTokensToCreateAsMintValue =
+    typeof tokensToGovernThreshold !== "undefined" ? new BN(getMintNaturalAmountFromDecimal(tokensToGovernThreshold, communityMintDecimals)) : DISABLED_VOTER_WEIGHT;
+
+  const realmPk = await withCreateRealm(
+    realmInstructions,
+    programIdPk,
+    programVersion,
+    realmName,
+    walletPk,
+    communityMintPk,
+    walletPk,
+    councilMintPk,
+    communityMaxVoteWeightSource,
+    minCommunityTokensToCreateAsMintValue,
+    communityTokenConfig,
+    councilTokenConfig
+  );
+
+  for (const teamWalletPk of councilWalletPks) {
+    // In version 3 we just deposit council tokens directly into the DAO
+    if (programVersion >= 3) {
+      // This is a workaround for an unnecessary signer check in DepositGoverningTokens.
+      if (teamWalletPk !== walletPk) {
+        await withCreateTokenOwnerRecord(realmInstructions, programIdPk, programVersion, realmPk, teamWalletPk, councilMintPk!, walletPk);
+      }
+
+      await withDepositGoverningTokens(
+        realmInstructions,
+        programIdPk,
+        programVersion,
+        realmPk,
+        councilMintPk!,
+        councilMintPk!,
+        teamWalletPk,
+        walletPk,
+        walletPk,
+        new BN(initialCouncilTokenAmount)
+      );
+      // TODO remove workaround once unnecessary signer bug in sdk is fixed
+      // this is a workaround
+      const buggedIx = realmInstructions[realmInstructions.length - 1];
+      // make teamWalletPk not a signer
+      buggedIx.keys = buggedIx.keys.map((key) => (key.pubkey.equals(teamWalletPk) && !key.pubkey.equals(walletPk) ? { ...key, isSigner: false } : key));
+    }
+
+    // before version 3, we have to mint the tokens to wallets
+    else {
+      const ataPk = await withCreateAssociatedTokenAccount(councilMembersInstructions, councilMintPk!, teamWalletPk, walletPk);
+
+      // Mint 1 council token to each team member
+      await withMintTo(councilMembersInstructions, councilMintPk!, ataPk, walletPk, initialCouncilTokenAmount);
+
+      if (teamWalletPk.equals(walletPk)) {
+        await withDepositGoverningTokens(
+          realmInstructions,
+          programIdPk,
+          programVersion,
+          realmPk,
+          ataPk,
+          councilMintPk!,
+          walletPk,
+          walletPk,
+          walletPk,
+          new BN(initialCouncilTokenAmount)
+        );
+      }
+    }
+  }
+
+  const { communityVoteThreshold, councilVoteThreshold, councilVetoVoteThreshold, communityVetoVoteThreshold } = createGovernanceThresholds(
+    programVersion,
+    communityYesVotePercentage,
+    councilYesVotePercentage
+  );
+
+  const VOTING_COOLOFF_TIME_DEFAULT = getTimestampFromDays(1);
+  // Put community and council mints under the realm governance with default config
+  const config = new GovernanceConfig({
+    communityVoteThreshold: communityVoteThreshold,
+    minCommunityTokensToCreateProposal: minCommunityTokensToCreateAsMintValue,
+    // Do not use instruction hold up time
+    minInstructionHoldUpTime: 0,
+    // maxVotingTime = baseVotingTime + votingCoolOffTime
+    // since this is actually baseVotingTime, we have to manually subtract the cooloff time.
+    baseVotingTime: getTimestampFromDays(maxVotingTimeInDays || 3) - VOTING_COOLOFF_TIME_DEFAULT,
+    communityVoteTipping: VoteTipping.Disabled,
+    councilVoteTipping: VoteTipping.Strict,
+    minCouncilTokensToCreateProposal: new BN(initialCouncilTokenAmount),
+    councilVoteThreshold: councilVoteThreshold,
+    councilVetoVoteThreshold: councilVetoVoteThreshold,
+    communityVetoVoteThreshold: communityVetoVoteThreshold,
+    votingCoolOffTime: VOTING_COOLOFF_TIME_DEFAULT,
+    depositExemptProposalCount: 10,
+  });
+
+  const mainGovernancePk = await withCreateGovernance(realmInstructions, programIdPk, programVersion, realmPk, undefined, config, PublicKey.default, walletPk, walletPk);
+
+  const nativeTreasuryAddress = await withCreateNativeTreasury(realmInstructions, programIdPk, programVersion, mainGovernancePk, walletPk);
+
+  if (transferCommunityMintAuthority) {
+    const ix = Token.createSetAuthorityInstruction(TOKEN_PROGRAM_ID, communityMintPk, nativeTreasuryAddress, "MintTokens", walletPk, []);
+    if (communityMintAccount?.account.freezeAuthority) {
+      const freezeMintAuthorityPassIx = Token.createSetAuthorityInstruction(TOKEN_PROGRAM_ID, communityMintPk, nativeTreasuryAddress, "FreezeAccount", walletPk, []);
+      realmInstructions.push(freezeMintAuthorityPassIx);
+    }
+    realmInstructions.push(ix);
+  }
+
+  if (councilMintPk && councilMintHasMintAuthority && transferCouncilMintAuthority) {
+    const ix = Token.createSetAuthorityInstruction(TOKEN_PROGRAM_ID, councilMintPk, nativeTreasuryAddress, "MintTokens", walletPk, []);
+    if (councilMintAccount?.account.freezeAuthority) {
+      const freezeMintAuthorityPassIx = Token.createSetAuthorityInstruction(TOKEN_PROGRAM_ID, councilMintPk, nativeTreasuryAddress, "FreezeAccount", walletPk, []);
+      realmInstructions.push(freezeMintAuthorityPassIx);
+    }
+    realmInstructions.push(ix);
+  }
+
+  // Set the community governance as the realm authority
+  if (!skipRealmAuthority) {
+    withSetRealmAuthority(realmInstructions, programIdPk, programVersion, realmPk, walletPk, mainGovernancePk, SetRealmAuthorityAction.SetChecked);
+  }
+
+  console.log(
+    mainGovernancePk,
+    communityMintPk,
+    councilMintPk,
+    realmPk,
+    realmInstructions,
+    realmSigners,
+    mintsSetupInstructions,
+    mintsSetupSigners,
+    councilMembersInstructions,
+    walletPk,
+    programIdPk,
+    programVersion,
+    minCommunityTokensToCreateAsMintValue
+  );
+};
+
+const parseMintMaxVoteWeight = (useSupplyFactor: boolean, communityMintDecimals: number, supplyFactor?: number, absoluteValue?: number) => {
+  if (useSupplyFactor) {
+    return supplyFactor
+      ? new MintMaxVoteWeightSource({
+          type: MintMaxVoteWeightSourceType.SupplyFraction,
+          value: new BN(new BigNumber(supplyFactor.toString()).shiftedBy(MintMaxVoteWeightSource.SUPPLY_FRACTION_DECIMALS).toString()),
+        })
+      : MintMaxVoteWeightSource.FULL_SUPPLY_FRACTION;
+  } else
+    return new MintMaxVoteWeightSource({
+      type: MintMaxVoteWeightSourceType.Absolute,
+      value: absoluteValue ? new BN(getMintNaturalAmountFromDecimal(absoluteValue, communityMintDecimals)) : new BN(1000),
+    });
+};
+
+function createGovernanceThresholds(
+  programVersion: number,
+  communityYesVotePercentage: "disabled" | number,
+  // ignored if program version < v3
+  councilYesVotePercentage?: "disabled" | number
+) {
+  // For backward compatybility with spl-gov versions <= 2
+  // for Council vote and Veto vote thresholds we have to pass YesVotePerentage(0)
+  const undefinedThreshold = new VoteThreshold({
+    type: VoteThresholdType.YesVotePercentage,
+    value: 0,
+  });
+
+  const communityVoteThreshold =
+    programVersion >= 3
+      ? communityYesVotePercentage !== "disabled"
+        ? new VoteThreshold({
+            value: communityYesVotePercentage,
+            type: VoteThresholdType.YesVotePercentage,
+          })
+        : new VoteThreshold({ type: VoteThresholdType.Disabled })
+      : new VoteThreshold({
+          value: communityYesVotePercentage as number,
+          type: VoteThresholdType.YesVotePercentage,
+        });
+
+  const councilVoteThreshold =
+    programVersion >= 3
+      ? councilYesVotePercentage !== "disabled" && councilYesVotePercentage !== undefined
+        ? new VoteThreshold({
+            value: councilYesVotePercentage,
+            type: VoteThresholdType.YesVotePercentage,
+          })
+        : new VoteThreshold({ type: VoteThresholdType.Disabled })
+      : undefinedThreshold;
+
+  const councilVetoVoteThreshold = programVersion >= 3 ? councilVoteThreshold : undefinedThreshold;
+
+  const communityVetoVoteThreshold = programVersion >= 3 ? new VoteThreshold({ type: VoteThresholdType.Disabled }) : undefinedThreshold;
+
+  return {
+    communityVoteThreshold,
+    councilVoteThreshold,
+    councilVetoVoteThreshold,
+    communityVetoVoteThreshold,
+  };
+}
