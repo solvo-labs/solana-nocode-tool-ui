7105c5450a7d1aa4c66b089d2dbe99d873bbf98e
afarukcali <afarukcali@gmail.com>
2023-10-12 16:01:06 +0300

    async sleep function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,7 @@ export const airdrop = async (connection: Connection, publicKey: PublicKey) => {
   const signature = await connection.requestAirdrop(publicKey, LAMPORTS_PER_SOL);
   await connection.confirmTransaction(signature);
 };
+
+export const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
