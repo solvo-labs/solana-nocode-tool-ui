cd1361f4471774a441b50e485ac7ecfeedda7ce9
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-10-12 15:21:53 +0300

    SearchInput.tsx: created

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,24 @@
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+
+type Props = {
+    onChange: () => void;
+}
+const SearchInput:React.FC<Props> = ({onChange}) => {
+    return (
+        <Paper
+            component="form"
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', height: "40px", borderRadius: "12px", width: "100%", border: "1px solid", borderColor: "gray", boxShadow: "none" }}>
+            <SearchIcon sx={{ p: '6px', color: "gray" }} />
+            <InputBase
+                sx={{ marginLeft: 1, flex: 1, fontSize: "0.75rem"}}
+                placeholder="Search Proposals"
+                onChange={onChange}
+            />
+        </Paper>
+    );
+};
+
+export default SearchInput;
