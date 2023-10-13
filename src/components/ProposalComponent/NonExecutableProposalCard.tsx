a156808e9406709d3fecd817e90c2cf112498dab
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-10-13 18:49:03 +0300

    NonExecutableProposal component

---
+++ src/components/ProposalComponent/NonExecutableProposalCard.tsx
@@ -0,0 +1,72 @@
+import { Card, CardContent, Typography, CardActions, Theme, Stack, Chip, Grid, Box, Divider } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import CustomizedProgressBars from "../CustomProgressBar";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    border: "1px solid gray",
+    borderRadius: "16px !important",
+    // height: "260px",
+    width: "100%",
+    "&:hover": {
+      backgroundColor: "whitesmoke !important",
+    },
+  },
+}));
+
+const NonExecutableProposalCard = () => {
+  const classes = useStyles();
+  return (
+    <CardActions
+      onClick={() => {
+        console.log("asdasd");
+      }}
+      sx={{ padding: "0" }}
+    >
+      <Card className={classes.card}>
+        <CardContent>
+          <Stack direction={"row"} justifyContent={"space-between"} alignContent={"center"} alignItems={"center"}>
+            <Typography>BASLIKKKKKKKK</Typography>
+            <Stack direction={"row"} spacing={2} justifyContent={"center"} alignItems={"center"}>
+              <ThumbUpIcon sx={{ fontSize: "24px" }} color="success"></ThumbUpIcon>
+              <Chip label="success" sx={{ height: "24px", fontSize: "12px" }} color="success" />
+            </Stack>
+          </Stack>
+          <Box sx={{ border: "1px solid black", borderRadius: "12px", maxWidth: "140px", justifyContent: "center", alignItems: "center" }}>
+            <Typography>12/12/2023</Typography>
+          </Box>
+          <Divider sx={{ marginTop: "0.5rem", backgroundColor: "gray" }}></Divider>
+          <Stack spacing={2} marginTop={"0.5rem"}>
+            <Grid container direction={"row"}>
+              <Stack direction={"row"} width={"100%"} justifyContent={"space-between"}>
+                <Stack direction={"row"} spacing={1} display={"flex"} alignItems={"baseline"} justifyContent={"flex-end"}>
+                  <Typography sx={{ fontSize: "0.8rem" }}>Secenek:1</Typography>
+                  <Typography sx={{ fontSize: "0.7rem" }}>0 votes</Typography>
+                </Stack>
+                <Typography sx={{ fontSize: "0.7rem" }}>%0</Typography>
+              </Stack>
+              <Grid item width={"100%"}>
+                <CustomizedProgressBars></CustomizedProgressBars>
+              </Grid>
+            </Grid>
+            <Grid container direction={"row"} marginTop={"0.5rem"}>
+              <Stack direction={"row"} width={"100%"} justifyContent={"space-between"}>
+                <Stack direction={"row"} spacing={1} display={"flex"} alignItems={"baseline"} justifyContent={"flex-end"}>
+                  <Typography sx={{ fontSize: "0.8rem" }}>Secenek:1</Typography>
+                  <Typography sx={{ fontSize: "0.7rem" }}>0 votes</Typography>
+                </Stack>
+                <Typography sx={{ fontSize: "0.7rem" }}>%0</Typography>
+              </Stack>
+              <Grid item width={"100%"}>
+                <CustomizedProgressBars></CustomizedProgressBars>
+              </Grid>
+            </Grid>
+          </Stack>
+        </CardContent>
+      </Card>
+    </CardActions>
+  );
+};
+
+export default NonExecutableProposalCard;
