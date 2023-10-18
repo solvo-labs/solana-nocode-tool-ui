868ad9227e1d546e0a41fa1b2a9927c1297095f5
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-10-18 17:33:26 +0300

    Login.tsx: fix item position

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { Grid, Theme, Typography } from "@mui/material";
+import { Grid, Stack, Theme, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import solanaLogoPng from "../assets/solana-logo.png";
 import { APP_NAME } from "../utils/enum";
@@ -10,6 +10,8 @@ import { APP_NAME } from "../utils/enum";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const useStyles = makeStyles((_theme: Theme) => ({
   outerContainer: {
+    width: "100%",
+    height: "100%",
     padding: "1rem",
     border: "1px solid linear-gradient(to right, #aa66fe, #23ed98)",
     background: "linear-gradient(to right, #aa66fe, #23ed98)",
@@ -35,16 +37,14 @@ const Login: React.FC = () => {
 
   return (
     <Grid container className={classes.outerContainer}>
-      <Grid container direction="column" className={classes.innerContainer}>
+      <Stack direction="column" className={classes.innerContainer} spacing={8}>
         {/* #7846B5 */}
         <Typography variant="h4" color="#5719A3">
           {APP_NAME.SOLANA}
         </Typography>
         <img className={classes.image} width={200} src={solanaLogoPng} />
-      </Grid>
-      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
         <WalletMultiButton className={classes.button} />
-      </div>
+      </Stack>
     </Grid>
   );
 };
